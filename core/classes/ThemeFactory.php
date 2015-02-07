edf343580db4434d76e22ab42327bb668f03f1b7
odanylevkyi <aleksey@danilevsky.com>
2015-02-07 20:15:26 +0200

    Update ThemeFactory.php

--- core/classes/ThemeFactory.php
+++ core/classes/ThemeFactory.php
@@ -2,11 +2,11 @@
 
 namespace core\classes;
 
-use core\interfaces\Factory;
+use core\interfaces\IFactory;
 use themes\app\AppTheme;
 use App;
 
-class ThemeFactory implements Factory {
+class ThemeFactory implements IFactory {
 	public static function create() {
 		$theme = 'themes\\'.App::$entity->mainTheme.'\\'.ucfirst(App::$entity->mainTheme).'Theme';
 		$theme::init();
