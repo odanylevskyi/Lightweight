6938963872a468d36b1e282eb709046075328433
Oleksii Danylevskyi <aleksey@danilevsky.com>
2015-02-07 09:52:16 +0200

    Add changes to View

--- core/classes/View.php
+++ core/classes/View.php
@@ -4,20 +4,12 @@ namespace core\classes;
 
 
 class View {
-	private $_controller;
-	private $_action;
 	private $_file;
 	
 	public function __construct($controller, $action) {
-		$this->_controller = $controller;
-		$this->_action = $action;
 		$this->_file = realpath('views/'.$controller.'/'.$action.'.php');
 	}
-	
-	public function getController() {
-		return $this->_controller;
-	}
-	
+		
 	public function renderFile($params = []) {
 		ob_start();
         ob_implicit_flush(false);
