3762e6736820f95e0aec9dc4603000e49d0cdca2
Oleksii Danylevskyi <aleksey@danilevsky.com>
2015-02-07 10:15:03 +0200

    Add future tasks

--- core/classes/Theme.php
+++ core/classes/Theme.php
@@ -5,11 +5,25 @@ namespace core\classes;
 use core\interfaces\ITheme;
 
 class Theme implements ITheme {
-	public static function register();
+	protected static $_css = [];
+	protected static $_js = [];
 	
-	public static function registerCssFile();
+	public static function register() {
+		foreach (self::$_css as $file) {
+			print self::registerCssFile($file);
+		}
+		foreach (self::$_js as $file) {
+			print self::registerJsFile($file);
+		}
+	}
+	
+	public static function registerCssFile($file) {
+		return Html::cssFile($file);
+	}
 	public static function registerCss();
 	
-	public static function registerJSFile();
+	public static function registerJSFile($file) {
+		return Html::jsFile($file);
+	}
 	public static function registerJS();
 }
--- core/interfaces/ITheme.php
+++ core/interfaces/ITheme.php
@@ -5,9 +5,9 @@ namespace core\interfaces;
 interface ITheme {
 	public static function register();
 	
-	public static function registerCssFile();
+	public static function registerCssFile($file);
 	public static function registerCss();
 	
-	public static function registerJSFile();
+	public static function registerJSFile($file);
 	public static function registerJS();
 }
